32dec85ff3c1303bd5d03bda5622cb47cb92e207
Alexander Collins <ac@Alexanders-MacBook-Pro.local>
2019-01-26 22:25:37 +1100

    Fixing import issue ignoring variables

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-
 let environment_variables = {};
 for(let i=0; i < Object.keys(process.env).length; ++i) {
-    let env_var_key = Object.keys(process.env)[i];
-    if(env_var_key.indexOf("REACT_APP") !== -1){
-        let key = env_var_key.replace('REACT_APP_', '');
-        environment_variables[key] = process.env[Object.keys(process.env)[i]];
-    }
+    environment_variables[Object.keys(process.env)[i].replace('REACT_APP_', '')] = process.env[Object.keys(process.env)[i]];
 }
 module.exports = environment_variables;
