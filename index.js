08308d619d6d3e80ccea1734fde5812493c7727d
Alexander Collins <ac@Alexanders-MacBook-Pro.local>
2019-01-27 20:13:46 +1100

    fixing foreach testing issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 function get_envs() {
     var environment_variables = {};
-    Object.keys(process.env).foreach(function(key){
+    Array.prototype.forEach.call(Object.keys(process.env), function(key){
         environment_variables[key.replace("REACT_APP_", "")] = process.env[key];
     });
     return environment_variables;
